9141e247d3cc5ea10e2984f4688b2ce850b0c242
Piotrek <amrozp@gmail.com>
2020-03-29 00:24:09 +0100

    init

---
+++ UWP/MainPage.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+/*using static UWP.DataStore;*/
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+           /* this.ViewModel = new DataStoreViewModel();*/
+
+        }
+
+        /*public DataStoreViewModel ViewModel { get; set; }*/
+    
+
+    private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
+
+/*namespace UWP
+{
+    public class DataStore
+    {
+        
+
+        private string fname = "PIOTR";
+        private string Summary;
+
+        public string FName
+        {
+            get { return this.fname; }
+            set
+            {
+                this.fname = value;
+                Summary = "";
+            }
+        }
+        private string lname;
+        public string LName
+        {
+            get { return this.lname; }
+
+            set
+            {
+                this.lname = value;
+                Summary = "";
+
+            }
+
+        }
+        public class DataStoreViewModel
+        {
+            private DataStore defaultDataStore = new DataStore();
+            public DataStore DefaultDataStore { get { return this.defaultDataStore; } }
+        }
+    }
+}*/
+
+/*namespace Quickstart
+{
+    public class Recording
+    {
+        public string ArtistName { get; set; }
+        public string CompositionName { get; set; }
+        public DateTime ReleaseDateTime { get; set; }
+        public Recording()
+        {
+            this.ArtistName = "Wolfgang Amadeus Mozart";
+            this.CompositionName = "Andante in C for Piano";
+            this.ReleaseDateTime = new DateTime(1761, 1, 1);
+        }
+        public string OneLineSummary
+        {
+            get
+            {
+                return $"{this.CompositionName} by {this.ArtistName}, released: "
+                    + this.ReleaseDateTime.ToString("d");
+            }
+        }
+    }
+    public class RecordingViewModel
+    {
+        private Recording defaultRecording = new Recording();
+        public Recording DefaultRecording { get { return this.defaultRecording; } }
+    }
+}*/
